9abc159015014c20292817552843ce4da1ece6f7
rohan11110 <rohanvimalking@gmail.com>
2025-01-13 01:09:29 +0530

    update for login

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const post = require('./model/post');
 const mongo_url = process.env.MONGO_URL;
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./model/user');
+const session = require('express-session');
+const userRouter = require('./routes/user');
+const flash = require("connect-flash");
+const ejsMate = require('ejs-mate');
+const {isLoggedIn} = require('./middleware.js');
 
+const sessionOptions = {
+    secret : "mysupersecretcode",
+    resave : false,
+    saveUninitialized : true,
+    cookie :{
+        expires : Date.now() + 7 * 24 * 60 * 60*1000,
+        maxAge : 7 *24 * 60 * 60*1000,
+        httpOnly : true,
+
+    }
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
 
 
 
@@ -17,6 +39,7 @@ app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.set("views",path.join(__dirname,"views"));
 
+app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 
 main().then(()=>{console.log("success!")})
@@ -30,7 +53,38 @@ app.listen(port, function () {
 });
 
 
+//Start-authentication
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    
+    next();
+    
+});
+
+
+app.use("/",userRouter);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// app.get("/demo",async(req, res) =>{
+//     let fakeUser = new User({
+//         email : "student@gmail.com",
+//         username : "student",
+//     });
+
+//    let registerdUser = await User.register(fakeUser,"helloworld");
+//    res.send(registerdUser);
+// });
+
+
+//End-authentication
 
 app.get("/", (req, res) => {
     res.redirect("/posts");  // Or render a specific view
@@ -43,8 +97,8 @@ app.get("/posts",async(req,res)=>{
 
 
 
-app.get("/posts/new",(req,res)=>{
-     
+app.get("/posts/new",isLoggedIn,(req,res)=>{
+   
     res.render("new.ejs");
     
 });
@@ -65,7 +119,7 @@ app.post("/posts/new",(req,res)=>{
     res.redirect('/posts');
 });
 
-app.get("/posts/:id/increment",async(req, res) => {
+app.get("/posts/:id/increment",isLoggedIn,async(req, res) => {
     let {id} = req.params;
     
     let newPost = await post.findById(id);
@@ -75,7 +129,7 @@ app.get("/posts/:id/increment",async(req, res) => {
     res.redirect('/posts');
 });
 
-app.get("/posts/:id/edit",async(req, res)=>{
+app.get("/posts/:id/edit",isLoggedIn,async(req, res)=>{
     let {id} = req.params;
     let newPost = await post.findById(id);
     res.render("edit.ejs",{newPost});
@@ -91,7 +145,7 @@ app.patch("/posts/:id/edit",async(req, res)=>{
 
 });
     
-app.delete("/posts/:id/delete",async(req, res)=>{
+app.delete("/posts/:id/delete",isLoggedIn,async(req, res)=>{
     let {id} = req.params;
     
     // let newPost = await post.findById(id);
---
+++ routes/user.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router  = express.Router();
+const User = require("../model/user");
+const passport = require('passport');
+
+router.get("/signup",(req,res)=>{
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", async (req, res) => {
+    try {
+        let { username, email, Password } = req.body;
+        const newUser = new User({ email, username });
+
+        let registeredUser = await User.register(newUser, Password);
+        req.login(registeredUser,(err)=>{
+            if(err) {
+                return next(err);
+            }
+            req.flash("success","User registerd");
+            res.redirect("/posts");
+        })
+        
+    } catch (e) {
+        req.flash("error","Error: " + e.message);
+        res.redirect("/signup");
+    }
+});
+
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",{failureRedirect : '/login',failureFlash : true}),async(req,res)=>{
+  req.flash("success","Welcome to the OpenPost");
+  res.redirect("/posts");
+});
+
+
+router.get("/logout",(req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+            return next(err);
+        }
+        req.flash("success","You are now logged out");
+        res.redirect("/posts");
+    })
+});
+module.exports = router;
